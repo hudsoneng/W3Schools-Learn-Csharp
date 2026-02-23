f165a8cb25efe6d9d6e53cda8497409ac85963cd
hudsonengineer <hudsonsouza@tutanota.com>
2026-02-23 20:00:32 -0300

    (#01)
    feat: new fundamentals level, Introduction to Csharp HOME

---
+++ Src/01-Fundamentals/01-Home/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Home
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /// <summary>
+            ///  Learn C#
+            /// C# (C-Sharp) is a programming language developed by Microsoft that runs on the .NET Framework.
+            /// C# is used to develop web apps, desktop apps, mobile apps, games and much more.
+            /// </summary>
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Src/01-Fundamentals/01-Home/README.md
@@ -0,0 +1,5 @@
+﻿# Learn C#
+
+## C# (C-Sharp) is a programming language developed by Microsoft that runs on the .NET Framework.
+
+## C# is used to develop web apps, desktop apps, mobile apps, games and much more.
