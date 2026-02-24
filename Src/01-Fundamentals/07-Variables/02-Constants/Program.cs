47742d0fdd9f19ddb27cad30cc62ac412f4886af
hudsonengineer <hudsonsouza@tutanota.com>
2026-02-24 16:32:37 -0300

    (#09)
    feat: lear Csharp Const

---
+++ Src/01-Fundamentals/07-Variables/02-Constants/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Constants
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /// <summary>
+            /// Use "const" if you want an immutable value.
+            /// </summary>
+            const int a = 15;
+        }
+    }
+}
---
+++ Src/01-Fundamentals/07-Variables/02-Constants/README.md
@@ -0,0 +1,9 @@
+﻿# C# Constants
+
+## Constants
+
+If you don't want others (or yourself) to overwrite existing values, you can add the const keyword in front of the variable type.
+
+This will declare the variable as "constant", which means unchangeable and read-only:
+
+Example specified In Program.cs
