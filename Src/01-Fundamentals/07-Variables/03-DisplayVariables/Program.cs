1d0d47a53ebc536a0a1312e8c0c48940b7463227
hudsonengineer <hudsonsouza@tutanota.com>
2026-02-25 10:40:37 -0300

    (10)
    feat: Displaying Variables

---
+++ Src/01-Fundamentals/07-Variables/03-DisplayVariables/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DisplayVariables
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /// <summary>
+            /// Example using Data Type string for display text and a variable
+            /// </summary>
+            string PersonName = "John";
+            Console.WriteLine($"Hello {PersonName}!");
+
+            /// <summary>
+            /// Example You can also use the + character to add a variable to another variable:
+            /// </summary>
+            string firstName = "John ";
+            string lastName = "Doe";
+            string fullName = firstName + lastName;
+            Console.WriteLine(fullName);
+
+
+            /// <summary>
+            /// Example For numeric values, the + character works as a mathematical operator (notice that we use int (integer) variables here):
+            /// </summary>
+            int x = 5;
+            int y = 6;
+            Console.WriteLine(x + y); // Print the value of x + y
+        }
+    }
+}
---
+++ Src/01-Fundamentals/07-Variables/03-DisplayVariables/README.md
@@ -0,0 +1,11 @@
+﻿
+# C-Sharp Display Variables
+
+## Display Variables
+
+<p>
+The WriteLine() method is often used to display variable values to the console window.
+To combine both text and a variable, use the + character:
+</p>
+
+Follow the Example in Program.cs, In <summmary>
