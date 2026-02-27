37cc96ba4b6e22c2e250b91f0aa4e5e4bf5fc401
hudsonengineer <hudsonsouza@tutanota.com>
2026-02-27 19:47:54 -0300

    (#11)
    Feat: Multiple variables

---
+++ Src/01-Fundamentals/07-Variables/04-MultipleVariables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MultipleVariables
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /// <summary>
+            ///  Declare Many Variables
+            ///  To declare more than one variable of the same type, use a comma-separated list:
+            /// </summary>
+
+            int x = 12, y = 25, z = 50;
+
+            Console.WriteLine($"{x} {y} {z}");
+        }
+    }
+}
---
+++ Src/01-Fundamentals/07-Variables/04-MultipleVariables/README.md
@@ -0,0 +1,13 @@
+﻿
+
+# C# Multiple Variables
+
+### Declare Many Variables
+
+To declare more than one variable of the same type, use a comma-separated list:
+
+Example:
+
+int x = 15, y = 10, z = 50;
+
+Console.WriteLine($"{x} {y} {z}");
